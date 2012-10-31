077bfc2f7870691224673d41434daf8f217ce517
alejandro.fernandez <alex11_campo@hotmail.com>
2012-10-31 11:16:39 +0000

    Clase event hecha

--- ws-events/trunk/ws-events-model/src/main/java/es/udc/ws/events/model/event/Event.java
+++ ws-events/trunk/ws-events-model/src/main/java/es/udc/ws/events/model/event/Event.java
@@ -1,18 +1,30 @@
 package es.udc.ws.events.model.event;
 
+import java.util.Calendar;
+
 public class Event {
 
     private Long eventId;
     private String name;
     private String description;
+    private Calendar fechaIni;
+    private Calendar fechaFin;
+    private boolean interno = true;
+    private String direccion;
+    private int aforo;
 
-    public Event(String name, String description) {
+    public Event(String name, String description,Calendar fechaIni,Calendar fechaFin,boolean interno, String direccion, int aforo) {
         this.name = name;
         this.description = description;
+        this.fechaIni = fechaIni;
+        this.fechaFin = fechaFin;
+        this.interno = interno;
+        this.direccion = direccion;
+        this.aforo = aforo;
     }
 
-    public Event(Long eventId, String name, String description) {
-        this(name, description);
+    public Event(Long eventId, String name, String description, Calendar fechaIni,Calendar fechaFin,boolean interno, String direccion, int aforo) {
+        this(name, description, fechaIni, fechaFin, interno,  direccion, aforo);
         this.eventId = eventId;
     }
 
@@ -40,50 +52,108 @@ public class Event {
         this.description = description;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((eventId == null) ? 0 : eventId.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result
-                + ((description == null) ? 0 : description.hashCode());
-        return result;
-    }
+    public Calendar getFechaIni() {
+		return fechaIni;
+	}
+
+	public void setFechaIni(Calendar fechaIni) {
+		this.fechaIni = fechaIni;
+	}
+
+	public Calendar getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Calendar fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public boolean isInterno() {
+		return interno;
+	}
+
+	public void setInterno(boolean interno) {
+		this.interno = interno;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getAforo() {
+		return aforo;
+	}
+
+	public void setAforo(int aforo) {
+		this.aforo = aforo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + aforo;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result
+				+ ((direccion == null) ? 0 : direccion.hashCode());
+		result = prime * result + ((eventId == null) ? 0 : eventId.hashCode());
+		result = prime * result
+				+ ((fechaFin == null) ? 0 : fechaFin.hashCode());
+		result = prime * result
+				+ ((fechaIni == null) ? 0 : fechaIni.hashCode());
+		result = prime * result + (interno ? 1231 : 1237);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Event other = (Event) obj;
-        if (eventId == null) {
-            if (other.eventId != null) {
-                return false;
-            }
-        } else if (!eventId.equals(other.eventId)) {
-            return false;
-        }
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        } else if (!name.equals(other.name)) {
-            return false;
-        }
-        if (description == null) {
-            if (other.description != null) {
-                return false;
-            }
-        } else if (!description.equals(other.description)) {
-            return false;
-        }
-        return true;
-    }
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Event other = (Event) obj;
+		if (aforo != other.aforo)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (direccion == null) {
+			if (other.direccion != null)
+				return false;
+		} else if (!direccion.equals(other.direccion))
+			return false;
+		if (eventId == null) {
+			if (other.eventId != null)
+				return false;
+		} else if (!eventId.equals(other.eventId))
+			return false;
+		if (fechaFin == null) {
+			if (other.fechaFin != null)
+				return false;
+		} else if (!fechaFin.equals(other.fechaFin))
+			return false;
+		if (fechaIni == null) {
+			if (other.fechaIni != null)
+				return false;
+		} else if (!fechaIni.equals(other.fechaIni))
+			return false;
+		if (interno != other.interno)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
--- ws-events/trunk/ws-events-model/src/main/java/es/udc/ws/events/model/event/MockSqlEventDao.java
+++ ws-events/trunk/ws-events-model/src/main/java/es/udc/ws/events/model/event/MockSqlEventDao.java
@@ -1,20 +1,73 @@
 package es.udc.ws.events.model.event;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.sql.Connection;
 
 import es.udc.ws.util.exceptions.InstanceNotFoundException;
 
 public class MockSqlEventDao implements SqlEventDao {
-
+	private Calendar fecha1 = Calendar.getInstance();
+	private Calendar fecha2 = Calendar.getInstance();
+	
+	
 	@Override
 	public Event create(Connection connection, Event event) {
-		return new Event(1L, event.getName(), event.getDescription());
+		return new Event(1L, event.getName(), event.getDescription(),event.getFechaIni(), event.getFechaFin(), event.isInterno(),event.getDireccion(),event.getAforo());
 	}
 
 	@Override
-	public Event find(Connection connection, Long eventId)
+	public void update(Connection connection, Long eventId)
 			throws InstanceNotFoundException {
-		return new Event(eventId, "event name", "event description");
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void delete(Connection connection, Long eventId)
+			throws InstanceNotFoundException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Event find_by_ID(Connection connection, Long eventId)
+			throws InstanceNotFoundException {
+		fecha1.set(2012,10,11);
+		fecha2.set(2012,10,14);
+		return new Event(eventId, "event name", "event description", fecha1,fecha2,true,"Calle Falsa 123",90);
+	}
+
+	
+
+	
+
+	@Override
+	public Event find(Connection connection, String clave, Calendar fecha)
+			throws InstanceNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int response(Connection connection, String username, Long eventId,
+			boolean asistencia) throws InstanceNotFoundException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList getResponses(Connection connection, Event evento,
+			boolean respuesta) throws InstanceNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList getResponses_by_ID(Connection connection, Long eventId)
+			throws InstanceNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
