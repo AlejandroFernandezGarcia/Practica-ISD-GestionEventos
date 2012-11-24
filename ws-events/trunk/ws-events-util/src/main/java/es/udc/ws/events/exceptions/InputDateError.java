f61d90ec243309fb83259420fd9f190aa73921f4
alejandro.fernandez <alejandro.fernandez@33035dff-90f6-4888-a8d3-e8b8aaba6898>
2012-11-24 19:30:58 +0000

    Eliminada alguna excepcion Modificado Propertyvaldiator

--- ws-events/trunk/ws-events-util/src/main/java/es/udc/ws/events/exceptions/InputDateError.java
+++
@@ -1,9 +0,0 @@
-package es.udc.ws.events.exceptions;
-
-@SuppressWarnings("serial")
-public class InputDateError extends Exception {
-
-    public InputDateError(String message) {
-        super(message);
-    }
-}
