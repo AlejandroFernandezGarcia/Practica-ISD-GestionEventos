1ed9c3211dce851f4352c4240be67540881d0da7
alejandro.fernandez <alejandro.fernandez@33035dff-90f6-4888-a8d3-e8b8aaba6898>
2012-12-07 00:16:19 +0000

    Completadas 2 operaciones mas de SOAP

---
+++ ws-events/trunk/ws-events-util/src/main/java/es/udc/ws/events/dto/ResponseDto.java
@@ -0,0 +1,57 @@
+package es.udc.ws.events.dto;
+
+import java.util.Calendar;
+
+public class ResponseDto {
+	private Long responseId;
+	private Long eventId;
+	private String username;
+	private boolean assists;
+	
+	public ResponseDto(Long responseId, Long eventId, String username, Boolean assists){
+		this.responseId = responseId;
+		this.eventId = eventId;
+		this.username = username;
+		this.assists = assists;
+	}
+
+	public Long getResponseId() {
+		return responseId;
+	}
+
+	public void setResponseId(Long responseId) {
+		this.responseId = responseId;
+	}
+
+	public Long getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(Long eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public boolean isAssists() {
+		return assists;
+	}
+
+	public void setAssists(boolean assists) {
+		this.assists = assists;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ResponseDto [responseId=" + responseId + ", eventId=" + eventId
+				+ ", username=" + username + ", assists=" + assists + "]";
+	}
+	
+}
