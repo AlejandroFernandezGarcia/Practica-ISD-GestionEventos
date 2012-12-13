7c851fc38aef09f6ef5620da45e158917a674f49
adrian.fernandez <adrian.fernandez@33035dff-90f6-4888-a8d3-e8b8aaba6898>
2012-12-13 23:17:42 +0000

    REST terminado a falta de que se resuelva el problema de List<Event> vacio o no?

--- ws-events/trunk/ws-events-service/src/main/java/es/udc/ws/events/restservice/servlets/EventsServlet.java
+++ ws-events/trunk/ws-events-service/src/main/java/es/udc/ws/events/restservice/servlets/EventsServlet.java
@@ -17,8 +17,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.httpclient.HttpStatus;
 import org.jdom.Document;
 import org.jdom.Element;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
 
 import es.udc.ws.events.dto.EventDto;
 import es.udc.ws.events.exceptions.EventRegisterUsersError;
@@ -27,6 +25,7 @@ import es.udc.ws.events.model.eventservice.EventServiceFactory;
 import es.udc.ws.events.util.EventToEventDtoConversor;
 import es.udc.ws.events.xml.XmlExceptionConversor;
 import es.udc.ws.events.xml.XmlEventDtoConversor;
+import es.udc.ws.events.xml.ParsingException;
 import es.udc.ws.util.exceptions.InputValidationException;
 import es.udc.ws.util.exceptions.InstanceNotFoundException;
 import es.udc.ws.util.servlet.ServletUtils;
@@ -34,97 +33,67 @@ import es.udc.ws.util.servlet.ServletUtils;
 @SuppressWarnings("serial")
 public class EventsServlet extends HttpServlet {
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		/*
+		 * Long eventId = new Long(0L);
+		 * 
+		 * String eventURL = req.getRequestURL().append("/").append(eventId)
+		 * .toString(); Map<String, String> headers = new HashMap<>(1);
+		 * headers.put("Location", eventURL);
+		 * 
+		 * ServletUtils.writeServiceResponse(resp, HttpStatus.SC_CREATED, null,
+		 * headers);
+		 * 
+		 * XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
+		 * outputter.output(getEventDocument(), resp.getOutputStream());
+		 */
+		EventDto xmlEvent;
+		try {
+			xmlEvent = XmlEventDtoConversor.toEvent(req.getInputStream());
+		} catch (ParsingException ex) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_BAD_REQUEST,
+							XmlExceptionConversor
+									.toInputValidationExceptionXml(new InputValidationException(
+											ex.getMessage())), null);
+
+			return;
+
+		}
+		Event evento = EventToEventDtoConversor.toEvent(xmlEvent);
+		try {
+			evento = EventServiceFactory.getService().addEvent(evento);
+		} catch (InputValidationException ex) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_BAD_REQUEST,
+							XmlExceptionConversor
+									.toInputValidationExceptionXml(new InputValidationException(
+											ex.getMessage())), null);
+			return;
+		}
+		EventDto eventDto = EventToEventDtoConversor.toEventDto(evento);
+
+		String eventURL = req.getRequestURL().append("/")
+				.append(evento.getEventId()).toString();
+		Map<String, String> headers = new HashMap<>(1);
+		headers.put("Location", eventURL);
+
+		ServletUtils.writeServiceResponse(resp, HttpStatus.SC_CREATED,
+				XmlEventDtoConversor.toXml(eventDto), headers);
+	}
+
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) 
             throws ServletException, IOException {
-    	
-        Long eventId = new Long(0L);
-
-        String eventURL = req.getRequestURL().append("/").append(eventId)
-                .toString();
-        Map<String, String> headers = new HashMap<>(1);
-        headers.put("Location", eventURL);
-
-        ServletUtils.writeServiceResponse(resp, HttpStatus.SC_CREATED, null,
-                headers);
-
-        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-        outputter.output(getEventDocument(), resp.getOutputStream());
-
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        /*
-        ServletUtils.writeServiceResponse(resp, HttpStatus.SC_OK,
-                "application/xml", null, null);
-        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-        outputter.output(getEventDocument(), resp.getOutputStream());
-        */
-    	String path = req.getPathInfo();
-        if(path == null || path.length() == 0 || "/".equals(path)) {
-            String clave = req.getParameter("clave");
-            String strDate = req.getParameter("inicio");
-            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-            Date date;
-            Calendar inicio = Calendar.getInstance();
-            Calendar fin = Calendar.getInstance();
-			try {
-				date = sdf.parse(strDate);
-				inicio.setTime(date);
-	            strDate = req.getParameter("fin");
-	            date = sdf.parse(strDate);
-	            fin.setTime(date);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-            
-            List<Event> listaEventos = EventServiceFactory.getService()
-                    .findEventByKeyword(clave,inicio,fin);
-            List<EventDto> listaEventosDtos = EventToEventDtoConversor
-                    .toEventDtos(listaEventos);
-            ServletUtils.writeServiceResponse(resp, HttpStatus.SC_OK,
-                    XmlEventDtoConversor.toXml(listaEventosDtos), null);
-        } else {
-            String eventIdAsString = path.endsWith("/") && path.length() > 2 ? 
-                    path.substring(1, path.length() - 1) : path.substring(1);
-            Long eventId;
-            try {
-                eventId = Long.valueOf(eventIdAsString);
-            } catch (NumberFormatException ex) {
-                ServletUtils.writeServiceResponse(resp, 
-                        HttpStatus.SC_BAD_REQUEST,
-                        XmlExceptionConversor.toInputValidationExceptionXml(
-                        new InputValidationException("Invalid Request: " +
-                            "parameter 'eventId' is invalid '" + 
-                            eventIdAsString + "'")),
-                        null);
-
-                return;
-            }
-             Event evento;
-            try {
-                evento = EventServiceFactory.getService().findEvent(eventId);
-            } catch (InstanceNotFoundException ex) {
-                ServletUtils.writeServiceResponse(resp, 
-                        HttpStatus.SC_NOT_FOUND, 
-                        XmlExceptionConversor.toInstanceNotFoundException(
-                new InstanceNotFoundException(
-                    ex.getInstanceId().toString(), ex.getInstanceType())),
-                    null);
-                return;
-            }
-            EventDto eventDto = EventToEventDtoConversor.toEventDto(evento);
-            ServletUtils.writeServiceResponse(resp, HttpStatus.SC_OK,
-                XmlEventDtoConversor.toXml(eventDto), null);
-        }
-    }
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+        
         String requestURI = req.getRequestURI();
-        int idx = requestURI.lastIndexOf('/');
-        if (idx <= 0) {
+        int index = requestURI.lastIndexOf('/');
+        if (index <= 0) {
             ServletUtils.writeServiceResponse(resp, HttpStatus.SC_BAD_REQUEST,
                     XmlExceptionConversor.toInputValidationExceptionXml(
                     new InputValidationException("Invalid Request: " + 
@@ -132,51 +101,206 @@ public class EventsServlet extends HttpServlet {
             return;
         }
         Long eventId;
-        String eventIdAsString = requestURI.substring(idx + 1);
+        String eventIdAsString = requestURI.substring(index + 1);
         try {
             eventId = Long.valueOf(eventIdAsString);
         } catch (NumberFormatException ex) {
             ServletUtils.writeServiceResponse(resp, HttpStatus.SC_BAD_REQUEST,
                     XmlExceptionConversor.toInputValidationExceptionXml(
                     new InputValidationException("Invalid Request: " + 
-                        "unable to parse event id '" + eventIdAsString + "'")),
-                    null);
-            
+                        "unable to parse event id '" + 
+                    eventIdAsString + "'")), null);
             return;
-        }
+        }        
+        
+        EventDto eventDto;
         try {
-            EventServiceFactory.getService().deleteEvent(eventId);
+            eventDto = XmlEventDtoConversor
+                    .toEvent(req.getInputStream());
+        } catch (ParsingException ex) {
+            ServletUtils.writeServiceResponse(resp, HttpStatus.SC_BAD_REQUEST, 
+                    XmlExceptionConversor.toInputValidationExceptionXml(
+                    new InputValidationException(ex.getMessage())), 
+                    null);
+            return;
+            
+        }
+        Event event = EventToEventDtoConversor.toEvent(eventDto);
+        event.setEventId(eventId);
+        try {
+            EventServiceFactory.getService().updateEvent(event);
+        } catch (InputValidationException ex) {
+            ServletUtils.writeServiceResponse(resp, HttpStatus.SC_BAD_REQUEST, 
+                    XmlExceptionConversor.toInputValidationExceptionXml(
+                    new InputValidationException(ex.getMessage())), 
+                    null);
+            return;
         } catch (InstanceNotFoundException ex) {
-           ServletUtils.writeServiceResponse(resp, HttpStatus.SC_NOT_FOUND,
-                   XmlExceptionConversor.toInstanceNotFoundException(
-                    new InstanceNotFoundException(
-                            ex.getInstanceId().toString(),
-                    ex.getInstanceType())), null);
+            ServletUtils.writeServiceResponse(resp, HttpStatus.SC_NOT_FOUND, 
+                    XmlExceptionConversor.toInstanceNotFoundException(
+                new InstanceNotFoundException(
+                    ex.getInstanceId().toString(), ex.getInstanceType())),
+                    null);       
             return;
         } catch (EventRegisterUsersError e) {
-        	ServletUtils.writeServiceResponse(resp, HttpStatus.SC_INTERNAL_SERVER_ERROR,
+        	ServletUtils.writeServiceResponse(resp, HttpStatus.SC_INTERNAL_SERVER_ERROR, 
                     XmlExceptionConversor.toEventRegisterUsersError(
-                     new EventRegisterUsersError("Error: Register user not found")),null);
-             return;
+                    new EventRegisterUsersError("Error: Register user not found")), 
+                    null);
+            return;
 		}
         ServletUtils.writeServiceResponse(resp, HttpStatus.SC_NO_CONTENT, 
-                null, null);
+                null, null);        
     }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		/*
+		 * ServletUtils.writeServiceResponse(resp, HttpStatus.SC_OK,
+		 * "application/xml", null, null); XMLOutputter outputter = new
+		 * XMLOutputter(Format.getPrettyFormat());
+		 * outputter.output(getEventDocument(), resp.getOutputStream());
+		 */
+		String path = req.getPathInfo();
+		if (path == null || path.length() == 0 || "/".equals(path)) {
+			String clave = req.getParameter("clave");
+			String strDate = req.getParameter("inicio");
+			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+			Date date;
+			Calendar inicio = Calendar.getInstance();
+			Calendar fin = Calendar.getInstance();
+			try {
+				date = sdf.parse(strDate);
+				inicio.setTime(date);
+				strDate = req.getParameter("fin");
+				date = sdf.parse(strDate);
+				fin.setTime(date);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 
-    private Document getEventDocument() {
+			List<Event> listaEventos = EventServiceFactory.getService()
+					.findEventByKeyword(clave, inicio, fin);
+			List<EventDto> listaEventosDtos = EventToEventDtoConversor
+					.toEventDtos(listaEventos);
+			ServletUtils.writeServiceResponse(resp, HttpStatus.SC_OK,
+					XmlEventDtoConversor.toXml(listaEventosDtos), null);
+		} else {
+			String eventIdAsString = path.endsWith("/") && path.length() > 2 ? path
+					.substring(1, path.length() - 1) : path.substring(1);
+			Long eventId;
+			try {
+				eventId = Long.valueOf(eventIdAsString);
+			} catch (NumberFormatException ex) {
+				ServletUtils
+						.writeServiceResponse(
+								resp,
+								HttpStatus.SC_BAD_REQUEST,
+								XmlExceptionConversor
+										.toInputValidationExceptionXml(new InputValidationException(
+												"Invalid Request: "
+														+ "parameter 'eventId' is invalid '"
+														+ eventIdAsString + "'")),
+								null);
 
-        Element eventElement = new Element("event");
+				return;
+			}
+			Event evento;
+			try {
+				evento = EventServiceFactory.getService().findEvent(eventId);
+			} catch (InstanceNotFoundException ex) {
+				ServletUtils
+						.writeServiceResponse(
+								resp,
+								HttpStatus.SC_NOT_FOUND,
+								XmlExceptionConversor
+										.toInstanceNotFoundException(new InstanceNotFoundException(
+												ex.getInstanceId().toString(),
+												ex.getInstanceType())), null);
+				return;
+			}
+			EventDto eventDto = EventToEventDtoConversor.toEventDto(evento);
+			ServletUtils.writeServiceResponse(resp, HttpStatus.SC_OK,
+					XmlEventDtoConversor.toXml(eventDto), null);
+		}
+	}
 
-        Element identifierElement = new Element("eventId");
-        identifierElement.setText(new Long(0L).toString());
-        eventElement.addContent(identifierElement);
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String requestURI = req.getRequestURI();
+		int idx = requestURI.lastIndexOf('/');
+		if (idx <= 0) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_BAD_REQUEST,
+							XmlExceptionConversor
+									.toInputValidationExceptionXml(new InputValidationException(
+											"Invalid Request: "
+													+ "unable to find event id")),
+							null);
+			return;
+		}
+		Long eventId;
+		String eventIdAsString = requestURI.substring(idx + 1);
+		try {
+			eventId = Long.valueOf(eventIdAsString);
+		} catch (NumberFormatException ex) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_BAD_REQUEST,
+							XmlExceptionConversor
+									.toInputValidationExceptionXml(new InputValidationException(
+											"Invalid Request: "
+													+ "unable to parse event id '"
+													+ eventIdAsString + "'")),
+							null);
 
-        Element nameElement = new Element("name");
-        nameElement.setText("event name");
-        eventElement.addContent(nameElement);
+			return;
+		}
+		try {
+			EventServiceFactory.getService().deleteEvent(eventId);
+		} catch (InstanceNotFoundException ex) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_NOT_FOUND,
+							XmlExceptionConversor
+									.toInstanceNotFoundException(new InstanceNotFoundException(
+											ex.getInstanceId().toString(), ex
+													.getInstanceType())), null);
+			return;
+		} catch (EventRegisterUsersError e) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_INTERNAL_SERVER_ERROR,
+							XmlExceptionConversor
+									.toEventRegisterUsersError(new EventRegisterUsersError(
+											"Error: Register user not found")),
+							null);
+			return;
+		}
+		ServletUtils.writeServiceResponse(resp, HttpStatus.SC_NO_CONTENT, null,
+				null);
+	}
+	
+//No se para que se usa....  lo voy a dejar por si acaso
+	private Document getEventDocument() {
 
-        Document document = new Document(eventElement);
-        return document;
+		Element eventElement = new Element("event");
 
-    }
+		Element identifierElement = new Element("eventId");
+		identifierElement.setText(new Long(0L).toString());
+		eventElement.addContent(identifierElement);
+
+		Element nameElement = new Element("name");
+		nameElement.setText("event name");
+		eventElement.addContent(nameElement);
+
+		Document document = new Document(eventElement);
+		return document;
+
+	}
 }
---
+++ ws-events/trunk/ws-events-service/src/main/java/es/udc/ws/events/restservice/servlets/ResponseServlet.java
@@ -0,0 +1,203 @@
+package es.udc.ws.events.restservice.servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.httpclient.HttpStatus;
+
+import es.udc.ws.events.dto.ResponseDto;
+import es.udc.ws.events.exceptions.EventRegisterUsersError;
+import es.udc.ws.events.exceptions.OverCapacityError;
+import es.udc.ws.events.model.eventservice.EventServiceFactory;
+import es.udc.ws.events.model.response.Response;
+import es.udc.ws.events.util.ResponseToResponseDtoConversor;
+import es.udc.ws.events.xml.XmlExceptionConversor;
+import es.udc.ws.events.xml.XmlResponseDtoConversor;
+import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
+import es.udc.ws.util.servlet.ServletUtils;
+
+public class ResponseServlet {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String eventIdParameter = req.getParameter("eventId");
+		if (eventIdParameter == null) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_BAD_REQUEST,
+							XmlExceptionConversor
+									.toInputValidationExceptionXml(new InputValidationException(
+											"Invalid Request: "
+													+ "parameter 'eventId' is mandatory")),
+							null);
+			return;
+		}
+		Long eventId;
+		try {
+			eventId = Long.valueOf(eventIdParameter);
+		} catch (NumberFormatException ex) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_BAD_REQUEST,
+							XmlExceptionConversor
+									.toInputValidationExceptionXml(new InputValidationException(
+											"Invalid Request: "
+													+ "parameter 'eventId' is invalid '"
+													+ eventIdParameter + "'")),
+							null);
+
+			return;
+		}
+		String userName = req.getParameter("userName");
+		if (userName == null) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_BAD_REQUEST,
+							XmlExceptionConversor
+									.toInputValidationExceptionXml(new InputValidationException(
+											"Invalid Request: "
+													+ "parameter 'userName' is mandatory")),
+							null);
+			return;
+		}
+		String strResponse = req.getParameter("respuesta");
+		if (strResponse == null) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_BAD_REQUEST,
+							XmlExceptionConversor
+									.toInputValidationExceptionXml(new InputValidationException(
+											"Invalid Request: "
+													+ "parameter 'respuesta' is mandatory")),
+							null);
+
+			return;
+		}
+		Boolean valueResponse = Boolean.getBoolean(strResponse);
+		Long responseId;
+		Response response;
+		try {
+			responseId = EventServiceFactory.getService().responseToEvent(
+					userName, eventId, valueResponse);
+			response = EventServiceFactory.getService().getResponsesByID(
+					responseId);
+		} catch (InstanceNotFoundException ex) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_NOT_FOUND,
+							XmlExceptionConversor
+									.toInstanceNotFoundException(new InstanceNotFoundException(
+											ex.getInstanceId().toString(), ex
+													.getInstanceType())), null);
+			return;
+		} catch (OverCapacityError ex) {
+			ServletUtils.writeServiceResponse(resp,
+					HttpStatus.SC_INTERNAL_SERVER_ERROR, XmlExceptionConversor
+							.toOverCapacityError(new OverCapacityError(
+									"Error: Event full")),
+					null);
+			return;
+		} catch (EventRegisterUsersError x) {
+			ServletUtils
+					.writeServiceResponse(
+							resp,
+							HttpStatus.SC_INTERNAL_SERVER_ERROR,
+							XmlExceptionConversor
+									.toEventRegisterUsersError(new EventRegisterUsersError(
+											"Error: Register user not found")),
+							null);
+			return;
+		}
+		ResponseDto responseDto = ResponseToResponseDtoConversor
+				.toResponseDto(response);
+
+		String responseURL = req.getRequestURL().append("/")
+				.append(response.getId()).toString();
+
+		Map<String, String> headers = new HashMap<>(1);
+		headers.put("Location", responseURL);
+
+		ServletUtils.writeServiceResponse(resp, HttpStatus.SC_CREATED,
+				XmlResponseDtoConversor.toXml(responseDto), headers);
+	}
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String path = req.getPathInfo();
+		if (path == null || path.length() == 0 || "/".equals(path)) {
+			Long eventId = Long.getLong(req.getParameter("eventId"));
+			Boolean valueResponse = Boolean.getBoolean(req
+					.getParameter("response"));
+			List<Response> listResponses;
+			try {
+				listResponses = EventServiceFactory.getService().getResponses(
+						eventId, valueResponse);
+			} catch (InstanceNotFoundException e) {
+				ServletUtils
+						.writeServiceResponse(
+								resp,
+								HttpStatus.SC_NOT_FOUND,
+								XmlExceptionConversor
+										.toInstanceNotFoundException(new InstanceNotFoundException(
+												e.getInstanceId().toString(), e
+														.getInstanceType())),
+								null);
+				return;
+			}
+			List<ResponseDto> listResponsesDtos = ResponseToResponseDtoConversor
+					.toResponseDtos(listResponses);
+			ServletUtils.writeServiceResponse(resp, HttpStatus.SC_OK,
+					XmlResponseDtoConversor.toXml(listResponsesDtos), null);
+		} else {
+			String responseIdAsString = path.endsWith("/") && path.length() > 2 ? path
+					.substring(1, path.length() - 1) : path.substring(1);
+			Long responseId;
+			try {
+				responseId = Long.valueOf(responseIdAsString);
+			} catch (NumberFormatException ex) {
+				ServletUtils
+						.writeServiceResponse(
+								resp,
+								HttpStatus.SC_BAD_REQUEST,
+								XmlExceptionConversor
+										.toInputValidationExceptionXml(new InputValidationException(
+												"Invalid Request: "
+														+ "parameter 'responseId' is invalid '"
+														+ responseIdAsString
+														+ "'")), null);
+
+				return;
+			}
+			Response response;
+			try {
+				response = EventServiceFactory.getService().getResponsesByID(
+						responseId);
+			} catch (InstanceNotFoundException ex) {
+				ServletUtils
+						.writeServiceResponse(
+								resp,
+								HttpStatus.SC_NOT_FOUND,
+								XmlExceptionConversor
+										.toInstanceNotFoundException(new InstanceNotFoundException(
+												ex.getInstanceId().toString(),
+												ex.getInstanceType())), null);
+				return;
+			}
+			ResponseDto responseDto = ResponseToResponseDtoConversor
+					.toResponseDto(response);
+			ServletUtils.writeServiceResponse(resp, HttpStatus.SC_OK,
+					XmlResponseDtoConversor.toXml(responseDto), null);
+		}
+	}
+}
