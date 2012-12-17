f49771626e2f3331ee7fe4cad833bae0a5d69428
alejandro.fernandez <alex11_campo@hotmail.com>
2012-12-17 21:01:27 +0000

    Properties editado y ClientService y factory hechos

--- ws-events/trunk/ws-events-client/src/main/java/es/udc/ws/events/client/service/ClientEventService.java
+++ ws-events/trunk/ws-events-client/src/main/java/es/udc/ws/events/client/service/ClientEventService.java
@@ -1,12 +1,30 @@
 package es.udc.ws.events.client.service;
 
+import java.util.Calendar;
+import java.util.List;
+
 import es.udc.ws.events.dto.EventDto;
+import es.udc.ws.events.dto.ResponseDto;
+import es.udc.ws.events.exceptions.EventRegisterUsersException;
+import es.udc.ws.events.exceptions.OverCapacityException;
 import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
 
 public interface ClientEventService {
 
     public Long addEvent(EventDto event) throws InputValidationException;
 
-    //public EventDto findEvent(Long eventId) throws InstanceNotFoundException;
-
+    public void updateEvent(EventDto event) throws InputValidationException, InstanceNotFoundException, EventRegisterUsersException;
+    
+    public void deleteEvent(Long eventId) throws InstanceNotFoundException, EventRegisterUsersException;
+    
+    public EventDto findEvent(Long eventId) throws InstanceNotFoundException;
+    
+    public List<EventDto> findEventByKeyword(String clave, Calendar fechaIni, Calendar fechaFin);
+    
+    public Long responseToEvent(String username, Long eventId, Boolean code) throws InstanceNotFoundException, OverCapacityException, EventRegisterUsersException;
+    
+    public List<ResponseDto> getResponses(Long eventId, Boolean code) throws InstanceNotFoundException;
+    
+    public ResponseDto getResponsesByID(Long responseId) throws InstanceNotFoundException;
 }
